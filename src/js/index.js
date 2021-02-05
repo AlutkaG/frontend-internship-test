ef8d52e27fdc616fa0764b771d4a05f69015d143
AlutkaG <alagerlach@interia.pl>
2021-02-05 10:36:14 +0100

    Task 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
 /* Here goes your JS code */
+
+var popup = document.getElementById("popup");
+var buttonOpen = document.getElementById("show-popup-form");
+var closeButton = document.getElementById("close");
+
+buttonOpen.onclick = function() {
+	popup.style.display = "flex";
+};
+
+closeButton.onclick = function() {
+	popup.style.display = "none";
+};
